5bb6a717f45128df3f9ec76a44d7fc4b1024a20b
Vanessa Wagner <52802563+vanessawagner@users.noreply.github.com>
2023-01-20 07:53:21 -0600

    first commit

---
+++ src/components/Home.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Home = () => {
+    return (
+        <div>
+                <h1>My Job Hunting Application</h1>
+                <hr/>
+                <p>This application allows a job seeker to keep track of interesting companies and open job applications.</p>
+                <p>Click the Companies button to view a list of hiring companies.</p>
+        </div>
+    )
+}
+
+export default Home
